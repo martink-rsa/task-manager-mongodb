897feb1b03f329b7f6b59668df4e7cdc0dd5e9c6
Martin Kruger <martin@martinkruger.com>
2021-01-24 21:27:48 +0200

    Cleaned up logs and unneeded code

--- src/index.js
+++ src/index.js
@@ -15,8 +15,6 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 3000;
 
-// app.use(auth);
-
 app.use(express.json());
 
 // Routers
@@ -26,26 +24,3 @@ app.use(taskRouter);
 app.listen(port, () => {
   log.info('Server is up on port:', port);
 });
-
-const Task = require('./models/task');
-const User = require('./models/user');
-
-const main = async () => {
-  const userId = '600d4e782a9804946ff5f142';
-  //
-  /* const task = await Task.findById('600d57f660c59997064c5a91');
-  await task.populate('owner').execPopulate();
-  console.log(task);
-  console.log(task.owner); */
-
-  // 600d4e782a9804946ff5f142
-
-  const user = await User.findById(userId);
-  await user.populate('tasks').execPopulate();
-  // console.log(user.tasks);
-};
-
-main();
-// Generate a token secret:
-/* const crypto = require('crypto');
-require('crypto').randomBytes(64).toString('hex') */
