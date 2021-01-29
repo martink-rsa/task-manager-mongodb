2a976e5848ba0994157bd4c1cafcaf4b139e26ba
Martin Kruger <martin@martinkruger.com>
2021-01-30 00:54:56 +0200

    Fixed MongoDB URL

--- src/mongodb.js
+++ src/mongodb.js
@@ -1,7 +1,7 @@
 const { MongoClient } = require('mongodb');
 const log = require('./utils/log');
 
-const connectionURL = 'mongodb://127.0.0.1:27017';
+const connectionURL = process.env.MONGODB_URL;
 
 MongoClient.connect(connectionURL, { useNewUrlParser: true }, (e) => {
   log.info('Attempting to connect to db: ' + connectionURL);
